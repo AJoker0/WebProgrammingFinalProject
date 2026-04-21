457c06e9634eaf7fe222d87a0b8d122b116b9e09
Andrii Titkov <andrii.titkov@gmail.com>
2026-04-21 12:57:14 +0300

    starting initialise our project

---
+++ ParadiseHotelFrontend/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Paradise Hotel is a luxurious hotel located in the heart of the city. We offer top-notch amenities and exceptional service to ensure our guests have a memorable stay.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ ParadiseHotelFrontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>Welcome to Paradise Hotel!</p>
+        </div>
+    );
+}
+export default Home;
